6ef2ab827ba40e8ca0b2961c3796d057e18134a1
Yi LIN <yilin.shcn@gmail.com>
2017-08-09 01:06:06 +0800

    section-3 slide-2 slide-3

---
+++ js/test-slick.js
@@ -0,0 +1,15 @@
+$(document).ready(function ($) {
+   
+
+   
+//start the slider
+    $(".slick").slick({
+      infinite: true,
+      arrows: false,
+      dots: true,
+      fade: true, 
+      speed: 500, 
+      cssEase: 'linear'
+    });
+
+      });
