c731ace808df59724024943e642c7506eba5de97
hfgl-yilin <biel.cslayman@gmail.com>
2018-09-07 23:07:38 +0800

    ribbon added & donation page & php based donation form

---
+++ donation.php
@@ -0,0 +1,12 @@
+
+<html>
+<head>
+<?php
+   require 'aws-sdk/aws-autoloader.php';
+?>
+</head>
+<body>
+Hi, dear <?php echo $_POST["subName"] ?>!<br>
+Thank you very much for your <?php echo $_POST["subNum"] ?> LOVE(s)!
+</body>
+</html>
