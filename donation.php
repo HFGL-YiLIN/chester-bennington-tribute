5ee7ac9e590dbb8b11ec026d4578f6a310402f00
hfgl-yilin <biel.cslayman@gmail.com>
2018-09-12 22:45:53 +0800

    donation form (jquery ajax & php & bootstrap)

--- donation.php
+++ donation.php
@@ -1,12 +1,50 @@
-
-<html>
-<head>
 <?php
-   require 'aws-sdk/aws-autoloader.php';
-?>
-</head>
-<body>
-Hi, dear <?php echo $_POST["subName"] ?>!<br>
-Thank you very much for your <?php echo $_POST["subNum"] ?> LOVE(s)!
-</body>
-</html>
+
+$name = '';
+
+$number = 0; 
+
+$okMessage = '';
+
+$errorMessage = 'Oops! Some error occurred. Please try again :-)'; 
+
+try
+{
+    if(count($_POST) == 0) throw new \Exception('Form is empty'); 
+
+    $name = $_POST["subName"];
+    $number = $_POST["subNum"];
+
+    $messageArray = array('$name' => $name, '$number' => $number); 
+
+    $template = 'Dear $name, thank you very much for your $number LOVE(s)!'; 
+    
+    $okMessage = strtr($template, $messageArray); 
+
+    $replyArray = array('type' => 'success', 'message' => $okMessage); 
+
+}
+catch (\Exception $e) 
+{
+    $replyArray = array('type' => 'danger', 'message' => $errorMessage); 
+}
+
+
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($replyArray);
+
+    header('Content-Type: application/json');
+
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $replyArray['message'];
+}
+
+
+
+
+
+?> 
